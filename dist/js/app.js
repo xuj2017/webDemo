68e3106a3abd683feff1632a2ca9545be810e749
xujun <xuj2017@outlook.com>
2017-04-23 21:08:19 +0800

    modify

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,2 +1,2 @@
-"use strict";var a=6,b=7;
+"use strict";var a=6,b=7;console.log(Number.isFinite(a));
 //# sourceMappingURL=app.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,3 @@
 let a = 6;
 let b = 7;
+console.log(Number.isFinite(a));
