1939cbf1e3ff979ecfaa15d4b2385118da8d7fe4
xujun <xuj2017@outlook.com>
2017-12-18 15:31:30 +0800

    modify

--- node/hello/src/hello.cc
+++
@@ -1,9 +0,0 @@
-#include <node.h>
-#include <v8.h>
-
-using namespace v8;
-
-Handle<Value> SayHello(const Arguments& args){
-    HandleScope scope;
-    return scope.Close(String::New('Hello world!'));
-}
--- node/test/app.js
+++ node/test/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -13,6 +14,13 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+app.use(session({
+  name:'xujtest',
+  secret:'dll',
+  cookie:('name','value',{'path':'/',httpOnly:true,secure:false,maxAge:60000}),
+  resave:true
+}))
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,6 +32,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+app.get('/count',function(req,res){
+  var sess = req.session;
+  console.log(sess);
+  if(sess.views){
+    sess.views++;
+    res.setHeader('Content-Type','text/html');
+    res.send('<p>欢迎第'+sess.views+'次访问   '+'expres in' + (sess.cookie.maxAge/1000)+'s</p>');
+  }else{
+    sess.views = 1;
+    res.send('请刷新');
+  }
+  
+
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
